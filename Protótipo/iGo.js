0738debd42d50aa8332d06f2381a1d1d7b94e24a
Luana Marques <luanadejesus1996@hotmail.com>
2019-03-30 19:36:42 +0000

    changes

--- Protótipo/iGo.js
+++ Protótipo/iGo.js
@@ -2,10 +2,7 @@ var display_element;
 
 var tNow;
 var tMin;
-var THour;
-
-
-
+var tHour;
 
 function start_time(){
 	var today = new Date();
@@ -15,13 +12,21 @@ function start_time(){
 	h = checkTime(h);
 	m = checkTime(m);
 
-	document.getElementById()
+	document.getElementById("clock_display").innerHTML = h + ":" + m;
+	tNow = m + h*100;
+	console.log("tNow = m + h*100 ="+ tNow);
+
+	tHour = h;
+	tMin = m;
+	var t = setTimeout(start_time, 500);
 }
 
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
 
 function start_display(){
 	display_element = document.getElementById("display");
-
 	start_time();
-
 }
